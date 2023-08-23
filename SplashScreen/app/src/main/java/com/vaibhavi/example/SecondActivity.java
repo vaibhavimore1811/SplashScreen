66521c2cf77fccaa5e7bdf99967e9e03391e1015
vaibhavimore <99947108+vaibhavimore1811@users.noreply.github.com>
2023-08-23 12:56:00 +0530

    Create SecondActivity.java

---
+++ SplashScreen/app/src/main/java/com/vaibhavi/example/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.vaibhavi.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
